2e35d62b9b585a5e8c94227d4420193a089afddb
SergeyShashin <s9997910@yandex.ru>
2026-01-22 16:44:55 +0300

    Algorithms. getSizeCrossingArrayUnicue updated 220126.

--- getSizeCrossingArrayUnicue/getSizeCrossingArrayUnicue.js
+++ getSizeCrossingArrayUnicue/getSizeCrossingArrayUnicue.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const dataForExample1 = [
+  [1, 2, 3],
+  [2, 3, 4, 5],
+  [2, 3]
+];
+
+const dataForExample2 = [
+  [1, 2, 3, 4],
+  [2, 3, 5],
+  [2, 3, 4, 6, 7]
+];
+
+const getSizeCrossingArrayUnicue = {
+  result: null,
+  run(data) {
+    this.result = {};
+    data.map(row => row.map(num => {
+      this.result[num] ? this.result[num]++ : this.result[num] = 1;
+    }
+    ));
+
+    console.log(this.result);
+  }
+}
+
+getSizeCrossingArrayUnicue.run(dataForExample1);
+getSizeCrossingArrayUnicue.run(dataForExample2);
